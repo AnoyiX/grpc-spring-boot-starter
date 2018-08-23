d7d3eb1f499df4e1160f88a80fa402c3ed3fed61
anoy <545544032@qq.com>
2018-08-23 17:21:30 +0800

    fix tcp timeout

--- spring-boot-starter-grpc/src/main/java/com/anoyi/grpc/ServerContext.java
+++ spring-boot-starter-grpc/src/main/java/com/anoyi/grpc/ServerContext.java
@@ -1,16 +1,20 @@
 package com.anoyi.grpc;
 
+import com.alibaba.fastjson.JSONObject;
+import com.anoyi.grpc.config.GrpcAutoConfiguration;
 import com.anoyi.grpc.service.CodecService;
 import com.anoyi.grpc.service.GrpcRequest;
 import com.anoyi.grpc.service.GrpcResponse;
-import com.anoyi.grpc.util.ProtobufUtils;
 import com.anoyi.rpc.CommonServiceGrpc;
 import com.anoyi.rpc.GrpcService;
 import com.google.protobuf.ByteString;
 import io.grpc.Channel;
-import org.springframework.util.SerializationUtils;
+
+import java.util.logging.Logger;
 
 public class ServerContext {
+
+    private static final Logger log = Logger.getLogger(GrpcAutoConfiguration.class.getName());
 
     private Channel channel;
 
@@ -28,13 +32,16 @@ public class ServerContext {
      * 处理 gRPC 请求
      */
     public GrpcResponse handle(GrpcRequest grpcRequest) {
-//        byte[] bytes = ProtobufUtils.serialize(grpcRequest);
         ByteString bytes = codecService.serialize(grpcRequest);
         GrpcService.Request request = GrpcService.Request.newBuilder().setRequest(bytes).build();
-        GrpcService.Response response = blockingStub.handle(request);
+        GrpcService.Response response;
+        try{
+            response = blockingStub.withWaitForReady().handle(request);
+        }catch (Exception e){
+            log.warning("GRPC handle error, re-handle: " + JSONObject.toJSONString(grpcRequest));
+            response = blockingStub.withWaitForReady().handle(request);
+        }
         return codecService.deserialize(response);
-//        ByteString responseBody = response.getResponse();
-//        return ProtobufUtils.deserialize(responseBody.toByteArray(), GrpcResponse.class);
     }
 
     /**
