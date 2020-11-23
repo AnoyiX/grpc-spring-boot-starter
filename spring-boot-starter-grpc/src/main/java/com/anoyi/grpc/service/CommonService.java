cd5ea0bd08607c6447dbe14e2a8e4934be395692
liguangbin826@163.com <Li3281905!>
2020-11-23 09:45:11 +0800

    1.修改了CommonService种的getBean方法，让可以通过类名找到对象 2.增加一个异常类，在handle方法中拦截他，这样使用者可以在自定义序列化或方法中返回自己的异常信息 3.升级到spring-boot 2.4.0，修改了版本号跟其一致，并更新了一些依赖版本，

--- spring-boot-starter-grpc/src/main/java/com/anoyi/grpc/service/CommonService.java
+++ spring-boot-starter-grpc/src/main/java/com/anoyi/grpc/service/CommonService.java
@@ -1,5 +1,6 @@
 package com.anoyi.grpc.service;
 
+import com.anoyi.grpc.exception.GrpcResponseException;
 import com.anoyi.grpc.util.SerializeUtils;
 import com.anoyi.rpc.CommonServiceGrpc;
 import com.anoyi.rpc.GrpcService;
@@ -23,6 +24,8 @@ public class CommonService extends CommonServiceGrpc.CommonServiceImplBase {
 
     private Map<Class, Object> serviceBeanMap = new ConcurrentHashMap<>();
 
+    private Map<String, Object> serviceStringMap = new ConcurrentHashMap<>();
+
     private final AbstractApplicationContext applicationContext;
 
     private final SerializeService defaultSerializationService;
@@ -36,26 +39,39 @@ public class CommonService extends CommonServiceGrpc.CommonServiceImplBase {
     public void handle(GrpcService.Request request, StreamObserver<GrpcService.Response> responseObserver) {
         int serialize = request.getSerialize();
         SerializeService serializeService = SerializeUtils.getSerializeService(serialize, defaultSerializationService);
-        GrpcRequest grpcRequest = serializeService.deserialize(request);
         GrpcResponse response = new GrpcResponse();
         try {
+            GrpcRequest grpcRequest = serializeService.deserialize(request);
             String className = grpcRequest.getClazz();
-            Object bean = getBean(Class.forName(className));
+            Object bean = getBean(className);
             Object[] args = grpcRequest.getArgs();
             Class[] argsTypes = getParameterTypes(args);
-            Method matchingMethod = MethodUtils.getMatchingMethod(Class.forName(className), grpcRequest.getMethod(), argsTypes);
+            Method matchingMethod = MethodUtils.getMatchingMethod(bean.getClass(), grpcRequest.getMethod(), argsTypes);
             FastClass serviceFastClass = FastClass.create(bean.getClass());
             FastMethod serviceFastMethod = serviceFastClass.getMethod(matchingMethod);
             Object result = serviceFastMethod.invoke(bean, args);
             response.success(result);
-        } catch (NoSuchBeanDefinitionException | ClassNotFoundException exception) {
+        }catch (NoSuchBeanDefinitionException | ClassNotFoundException exception) {
             String message = exception.getClass().getName() + ": " + exception.getMessage();
             response.error(message, exception, exception.getStackTrace());
             log.error("method not implement", exception.getCause());
-        } catch (InvocationTargetException exception) {
-            String message = exception.getCause().getClass().getName() + ": " + exception.getCause().getMessage();
-            response.error(message, exception.getCause(), exception.getCause().getStackTrace());
-            log.error("method invoke error", exception.getCause());
+        } catch (GrpcResponseException | InvocationTargetException exception) {
+            //通过GrpcResponseException，可以在序列化或方法内部做自定义的错误返回
+            GrpcResponseException grpcException = null;
+            if(exception instanceof InvocationTargetException
+                    && ((InvocationTargetException) exception).getTargetException() instanceof GrpcResponseException){
+                grpcException = (GrpcResponseException)((InvocationTargetException) exception).getTargetException();
+            }
+            if(exception instanceof GrpcResponseException){
+                grpcException = (GrpcResponseException)exception;
+            }
+            if(grpcException != null && grpcException.getResponse() != null){
+                response = grpcException.getResponse();
+            }else {
+                String message = exception.getCause().getClass().getName() + ": " + exception.getCause().getMessage();
+                response.error(message, exception.getCause(), exception.getCause().getStackTrace());
+                log.error("method invoke error", exception.getCause());
+            }
         }
         ByteString bytes = serializeService.serialize(response);
         GrpcService.Response grpcResponse = GrpcService.Response.newBuilder().setResponse(bytes).build();
@@ -66,17 +82,27 @@ public class CommonService extends CommonServiceGrpc.CommonServiceImplBase {
     /**
      * 获取 Service Bean
      */
-    private Object getBean(Class clazz) throws NoSuchBeanDefinitionException {
-        if (serviceBeanMap.containsKey(clazz)) {
-            return serviceBeanMap.get(clazz);
-        }
+    private Object getBean(String className) throws NoSuchBeanDefinitionException, ClassNotFoundException {
+        Object bean = null;
         try {
-            Object bean = applicationContext.getBean(clazz);
-            serviceBeanMap.put(clazz, bean);
-            return bean;
+            if(className.indexOf(".") > -1){
+                Class clazz = Class.forName(className);
+                if (serviceBeanMap.containsKey(clazz)) {
+                    return serviceBeanMap.get(clazz);
+                }
+                bean = applicationContext.getBean(clazz);
+                serviceBeanMap.put(clazz, bean);
+            }else{
+                if (serviceStringMap.containsKey(className)) {
+                    return serviceStringMap.get(className);
+                }
+                bean = applicationContext.getBean(className);
+                serviceStringMap.put(className, bean);
+            }
         } catch (BeansException e) {
-            throw new NoSuchBeanDefinitionException(clazz);
+            throw new NoSuchBeanDefinitionException(className);
         }
+        return bean;
     }
 
     /**
